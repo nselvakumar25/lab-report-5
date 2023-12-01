bb8b14598869940a5b77f8aa37e427834f00c342
nselvakumar25 <146896734+nselvakumar25@users.noreply.github.com>
2023-11-30 16:24:13 -0800

    Update ListExamples.java

--- ListExamples.java
+++ ListExamples.java
@@ -12,9 +12,10 @@ class ListExamples {
     List<String> result = new ArrayList<>();
     for(String s: list) {
       if(sc.checkString(s)) {
-        result.add(0, s);
+        list.add(s);
       }
     }
+    System.out.println(result);
     return result;
   }
 
